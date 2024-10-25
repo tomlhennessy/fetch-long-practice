a2b96527ce619abbfd73a7d5db6c62adb47b9668
Tom Hennessy <tomlhennessy@outlook.com>
2024-10-26 08:50:41 +1000

    code for fetch requests made in browser

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,16 +5,74 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-// Your code here
-
+fetch("http://localhost:5001/products", {
+    method: "POST",
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
+.then(response => response.text())
+.then(data => console.log("Product created:", data))
+.catch(error => console.error("Error:", error));
 
 
 /* ============================== Phase 2 ============================== */
 
-// Your code here
+fetch("http://localhost:5001/products", {
+    method: "POST",
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
+.then(response => {
+    console.log("Status Code:", response.status);
+    console.log("Content-Type:", response.headers.get("Content-Type"));
+    console.log("Response URL:", response.url);
+    console.log("Redirected?", response.redirected);
+})
+.catch(error => console.error("Error:", error));
 
 
 
 /* ============================== Phase 3 ============================== */
 
-// Your code here
+const body = new URLSearchParams({
+    name: "Caribbean Delight Coffee",
+    description: "Made by Manatee Coffee",
+    price: "11.99",
+    categories: "grocery"
+})
+
+fetch("http://localhost:5001/products", {
+    method: "POST",
+    body: body,
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
+.then(response => response.text())
+.then(data => console.log("Product created", data))
+.catch(error => console.error("Error:", error));
+
+
+
+/* ============================== Phase 4 ============================== */
+
+// fetch a specific product
+fetch("http://localhost:5001/products/1")
+    .then(response => response.text())
+    .then(data => console.log("Product details", data))
+    .catch(error => console.error("Error:", error));
+
+
+
+// delete a product
+
+fetch("http://localhost:5001/products/1/delete", {
+    method: "POST"
+})
+.then(response => response.text())
+.then(data => console.log("Product deleted:", data))
+.catch(error => console.error("Error:", error));
